44ecfe8780b0bc0a39461c8666744a24520d4afd
Anuj Maurya <anujmaurya@cedcoss.com>
2023-03-16 17:26:49 +0530

    subtasks 100-110 completed

--- JS/script.js
+++ JS/script.js
@@ -152,3 +152,122 @@ let total = 0;
 for(let i = 0; i < myArr.length; i++) {
   total += myArr[i];
 }
+
+// 101. Nesting For Loops
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for(let i = 0; i < arr.length; i++) {
+      for(let j = 0; j < arr[i].length; j++) {
+        product *= arr[i][j];
+      }
+    }
+    // Only change code above this line
+    return product;
+  }
+  
+  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+// 102. Iterate with JavaScript Do...While Loops
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do  {
+  myArray.push(i);
+  i++;
+} while(i < 5)
+
+// 103. Replace Loops using Recursion
+function sum(arr, n) {
+    // Only change code below this line
+    if(n <= 0) {
+      return 0;
+    } else {
+      return sum(arr, n-1) + arr[n-1];
+    }
+    // Only change code above this line
+  }
+//   104. Profile Lookup
+// Setup
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  function lookUpProfile(name, prop) {
+    // Only change code below this line
+    let n = name.length;
+   for (let i = 0; i < n; i++) {
+      if (contacts[i].firstName == name) {
+        if (contacts[i].hasOwnProperty(prop)) {
+          return contacts[i][prop];
+        } else {
+          return "No such property";
+        }
+      }
+    }
+    return "No such contact";
+    // Only change code above this line
+  }
+  
+  lookUpProfile("Akira", "likes");
+
+// 105. Generate Random Fractions with JavaScript
+function randomFraction() {
+    return Math.random();
+}
+
+// 106. Generate Random Whole Numbers with JavaScript
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10);
+}
+
+// 107. Generate Random Whole Numbers within a Range
+function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
+
+// 108. Use the parseInt Function
+function convertToInteger(str) {
+    return parseInt(str);
+  }
+  
+  convertToInteger("56");
+
+// 109. Use the parseInt Function with a Radix
+function convertToInteger(str) {
+    const a = parseInt(str, 2);
+    return a;
+  }
+  
+  convertToInteger("10011");
+
+// 110 .Use the Conditional (Ternary) Operator
+function checkEqual(a, b) {
+    return (a == b) ? "Equal" : "Not Equal";
+  }
+  checkEqual(1, 2);
